6d700fd35f55e7193b287e6dadf7587090a7a3e2
brkamrud <brkamrud@gmail.com>
2016-09-06 10:51:38 -0700

    Added Files

---
+++ APLesson_01/ASCIIArt1.java
@@ -0,0 +1,16 @@
+public class ASCIIArt1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  ,_         _,");
+		System.out.println("  | \\\\___//|");
+		System.out.println("  |=6   6=|");
+		System.out.println(" \\=._Y_.=/");
+		System.out.println("   )    `     (      ,");
+		System.out.println("  /             \\   ((");
+		System.out.println(" |               |    ))");
+		System.out.println("/| |          | |\\_//");
+		System.out.println("\\| | .___.| |/ -`");
+		System.out.println("  '\"'           '\"'");
+	}
+}
---
+++ APLesson_01/ASCIIArt2.java
@@ -0,0 +1,20 @@
+public class ASCIIArt2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("   ____________ ");
+		System.out.println("<____________>");
+		System.out.println("  |                       |");
+		System.out.println("  |                       |");
+		System.out.println("  |                       |");
+		System.out.println("   \\                     / ");
+		System.out.println("     \\_________/  ");
+		System.out.println("             |  |");
+		System.out.println("             |  |");
+		System.out.println("             |  |");
+		System.out.println("             |  |");
+		System.out.println("       ___|  |___");
+		System.out.println("     /       |  |      \\");
+		System.out.println("     \\_________/");
+	}
+}
