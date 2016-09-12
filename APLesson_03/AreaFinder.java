03c08ea9eca5067737b8ae13fdf45d368d3217d6
brkamrud <brkamrud@gmail.com>
2016-09-12 10:49:18 -0700

    Added Lesson 3 Stuff

--- APLesson_03/AreaFinder.java
+++ APLesson_03/AreaFinder.java
@@ -4,6 +4,17 @@ public class AreaFinder
 {
 	public static void main(String[]args)
 	{
+		Scanner keyboard = new Scanner(System.in);
+		double w;
+		double l;
 		
+		System.out.println("Please enter your width.");
+		w = keyboard.nextDouble();
+		
+		System.out.println("Please enter your length.");
+		l = keyboard.nextDouble();
+		
+		Double area = w * l;
+		System.out.println("Your area is " + area + ".");
 	}
 }
