015046d26c8089dd1ebc121f29d544a5f84d2d13
brkamrud <brkamrud@gmail.com>
2016-10-18 11:47:31 -0700

    Added Lesson 5.2 Stuff

--- APLesson_05/Password.java
+++ APLesson_05/Password.java
@@ -6,9 +6,27 @@ public class Password
 	{
 		Scanner kb = new Scanner(System.in);
 		
+		System.out.println("Please enter your username:");
+		String user = kb.nextLine();
+		System.out.println("Please enter your password:");
+		String pass = kb.nextLine();
 		
+		passCheck(user, pass);
 		
-		
-		
+	}
+	
+	public static void passCheck(String user, String pass)
+	{
+		if (user.equals("kamrudb9567") && pass.equals("brett123"))
+			System.out.println("You are granted access!");
+		else
+			if (!user.equals("kamrudb9567") && pass.equals("brett123"))
+				System.out.println("Your username is incorrect!");
+		else
+			if (user.equals("kamrudb9567") && !pass.equals("brett123"))
+				System.out.println("Your password is incorrect!");
+		else
+			if (!user.equals("kamrudb9567") && !pass.equals("brett123"))
+				System.out.println("Your username and password is incorrect!");
 	}
 }
