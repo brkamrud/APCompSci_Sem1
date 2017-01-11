c4524c7a7b54954457c8559e330a201135620999
brkamrud <brkamrud@gmail.com>
2017-01-11 11:54:59 -0800

    Fixed Password

--- APLesson_05/Password.java
+++ APLesson_05/Password.java
@@ -4,6 +4,12 @@ public class Password
 {
 	public static void main(String[]args)
 	{
+		//Scanner kb = new Scanner(System.in);
+		passCheck();
+	}
+	
+	public static void passCheck()
+	{
 		Scanner kb = new Scanner(System.in);
 		
 		System.out.println("Please enter your username:");
@@ -11,22 +17,25 @@ public class Password
 		System.out.println("Please enter your password:");
 		String pass = kb.nextLine();
 		
-		passCheck(user, pass);
-		
-	}
-	
-	public static void passCheck(String user, String pass)
-	{
 		if (user.equals("kamrudb9567") && pass.equals("brett123"))
 			System.out.println("You are granted access!");
 		else
 			if (!user.equals("kamrudb9567") && pass.equals("brett123"))
+			{
 				System.out.println("Your username is incorrect!");
+				passCheck();
+			}	
 		else
 			if (user.equals("kamrudb9567") && !pass.equals("brett123"))
+			{
 				System.out.println("Your password is incorrect!");
+				passCheck();
+			}
 		else
 			if (!user.equals("kamrudb9567") && !pass.equals("brett123"))
+			{
 				System.out.println("Your username and password is incorrect!");
+				passCheck();
+			}
 	}
 }
