63aedbbb4141e39512015959b8907324b1b1c7a0
brkamrud <brkamrud@gmail.com>
2017-02-16 11:58:33 -0800

    Added Lesson 13 Stuff

--- APLesson_13/ToyStore.java
+++ APLesson_13/ToyStore.java
@@ -1,18 +1,51 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ToyStore
 {
-	private ArrayList<Toy> toyList = new ArrayList<Toy>();
+	private ArrayList<String> toyList = new ArrayList<String>();
 	
 	//DC
 	public ToyStore()
 	{
-		toyList = "";
+		this.toyList = new ArrayList<String>();
 	}
 	
 	//C w/ P
-	public ToyStore(ArrayList<Toy> toys)
+	public ToyStore(String toys)
 	{
-		toyList = toys;
+		loadToys(toys);
+	}
+	
+	public void loadToys(String ts)
+	{
+		String[] list = ts.split(", ");
+		ArrayList<String> toys = new ArrayList<String>(Arrays.asList(list));
+		
+		for (int i = 0; i < toys.size(); i++)
+		{
+			String name = toys.get(i);
+			String type = toys.get(i + 1);
+			if (getThatToy(name) == null)
+			{
+				if (type.equals("Car"))
+					toyList.add("Car");
+				else if (type.equals("AF"))
+					toyList.add("AFigure");
+			}
+			else
+				i++;
+		}
+	}
+	
+	public String getThatToy(String nm)
+	{
+		for (String toy : toyList)
+		{
+			if (toy.equals(nm))
+				return toy;
+			else
+				return null;
+		}
 	}
 }
