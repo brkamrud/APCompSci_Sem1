24cf52375befc97ac406b7b2a9eee933416a173c
brkamrud <brkamrud@gmail.com>
2017-03-10 12:07:46 -0800

    Added Lesson 14 Stuff

---
+++ APLesson_14/Navigation System/Honda.java
@@ -0,0 +1,26 @@
+public class Honda extensd Car
+{
+	private double[] location = new double[2];
+	
+	public Honda(double[] loc)
+	{
+		location[0] = loc[0];
+		location[1] = loc[1];
+	}
+	
+	public int getID()
+	{
+		return (int)(Math.random() * ((999999 - 100000) + 1)) + 100000;
+	}
+	
+	public void move(double x, double y)
+	{
+		x = x;
+		y = y;
+	}
+	
+	public double[] getLoc()
+	{
+		return location;
+	}
+}
--- APLesson_14/Navigation System/Toyota.java
+++ APLesson_14/Navigation System/Toyota.java
@@ -1,16 +1,34 @@
-public class Toyota implements Location
+public class Toyota extends Car
 {
 	private double[] location = new double[2];
 	
 	public Toyota()
 	{
 		location[0] = 0;
-		location[1] = 1;
+		location[1] = 0;
 	}
 	
 	public Toyota(String coordinates)
 	{
-		String[] cd = coordinates.split(", ");
-		location = Double.parseDouble(cd);
+		String[] cd =  new String[2];
+		cd = coordinates.split(", ");
+		location[0] = Double.parseDouble(cd[0]);
+		location[1] = Double.parseDouble(cd[1]);
+	}
+	
+	public int getID()
+	{
+		return (int)(Math.random() * ((999999 - 100000) + 1)) + 100000;
+	}
+	
+	public void move(double x, double y)
+	{
+		x = location[0];
+		y = location[1];
+	}
+	
+	public double[] getLoc()
+	{
+		return location;
 	}
 }
