4d1dd0dc6bc78f5762d065c30978b170df462f76
brkamrud <brkamrud@gmail.com>
2017-03-14 12:10:27 -0700

    Added Lesson 14 Stuff

--- APLesson_14/Navigation System/Car.java
+++ APLesson_14/Navigation System/Car.java
@@ -1,4 +1,22 @@
 public abstract class Car implements Location
 {
+	private double[] location = new double[2];
+	public final int ID = (int)(Math.random() * ((999999 - 100000) + 1)) + 100000;
 	
+	public Car()
+	{
+		double[] loc = location.clone(); 
+	}
+	
+	public int getID()
+	{
+		return ID;
+	}
+	
+	public abstract void move(double x, double y);
+	
+	public double[] getLoc()
+	{
+		return location;
+	}
 }
